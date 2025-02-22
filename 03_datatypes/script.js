c0f0d8d53bad04f23aac58275bf6d15b8e7b7add
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-02-22 22:18:01 +0530

    Datatypes

---
+++ 03_datatypes/script.js
@@ -0,0 +1,47 @@
+// Datatype
+// number
+let num1 = 3.14
+let num2 = 10
+
+console.log(num1 + num2);
+console.log(typeof(num1));
+
+// string
+const firstname = "Pro"
+console.log(firstname);
+
+let str = 'str123@ab#cd'
+
+// boolean 1 | 0
+let isLoggedin = true
+console.log(isLoggedin + 10); // 11 //as true -> 1, false-> 0
+
+// Null
+let lastLoginDate = null;
+
+console.log(lastLoginDate);
+
+//Object
+const person = {
+    firstName: "Pratyusha",
+    lastName: "Bhandary",
+    age: 21,
+    isLoggedIn: false
+}
+
+console.log(person);
+console.log(person.firstName);
+
+// string '1' + '1' - string
+// string + number = string
+// string * number = number
+console.log('a' * 1); // NaN
+console.log('a' + 1); // a1
+
+
+// typeof operator
+console.log(typeof(num1));
+
+
+
+

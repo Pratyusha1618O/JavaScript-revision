ad4c146994c3eba03fefdcd570871a1193de5ad5
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-02-22 22:17:40 +0530

    Variables

---
+++ 02_variable/script.js
@@ -0,0 +1,35 @@
+// variables
+
+//datatype variable = value; Strongly typed language
+// JS => loosely typed language
+
+var age = 21
+
+console.log(age);
+
+age = "Tithi";
+console.log(age);
+
+// var => global
+if (true){
+    var age = 23;
+}
+console.log(age);
+
+//...................................
+// let => local
+// can be re-assigned
+let roll = 1
+console.log(roll);
+
+
+//...................................
+// const
+// scope -> local
+
+const id = 101
+// id = 102 // can not be assigned again
+console.log(id);
+
+
+

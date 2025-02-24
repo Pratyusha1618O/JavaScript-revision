b6e01a7bdda1010e5a1ebcc0095378a9e3e41aa2
Pratyusha1618O <pratyushabhandary2003@gmail.com>
2025-02-24 20:28:54 +0530

    API calling

--- 14_API/script.js
+++ 14_API/script.js
@@ -6,7 +6,7 @@ const cityName = document.getElementById("city")
 const time = document.getElementById("time")
 const temp = document.getElementById("temp")
 
-async function getData(cityName){
+async function getData(cityName){ // takes city name, fetch reports, returns json
     const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=14442cab8be047b1a7245234240805&q=${cityName}&aqi=yes`);
 
     return promise.json()
@@ -15,7 +15,8 @@ async function getData(cityName){
 
 btn.addEventListener('click', async()=>{
     const value = input.value;
-    const result = await getData(value);
+    const result = await getData(value); // calling getData function, It calls API and fetch data
+
     cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
     time.innerText = result.location.localtime;
     temp.innerText = result.current.temp_c;
